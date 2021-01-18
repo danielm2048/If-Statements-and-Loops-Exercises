7b6b83856f188f6e412afe276dd3cedebb9dd5e3
danielm2048 <danielmimoun98@gmail.com>
2021-01-18 17:18:32 +0200

    Second answer

---
+++ q2.js
@@ -0,0 +1,6 @@
+let num1 = prompt("Insert first number");
+let num2 = prompt("Insert second number");
+let num3 = prompt("Insert third number");
+let product = num1 * num2 * num3;
+
+alert(product < 0 ? "-" : "+");
